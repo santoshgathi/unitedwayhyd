b085e391ff5b518265da6abf447b833774164e4e
Aditya-gathi <adityanathc@gathi.com>
2020-05-25 15:49:34 +0530

    80g send email

--- application/controllers/Eightyg.php
+++ application/controllers/Eightyg.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 require(APPPATH .'third_party/fpdf/fpdf.php');
+require(APPPATH .'third_party/phpmailer/PHPMailerAutoload.php');
 
 class Eightyg extends MY_Controller {
 
@@ -37,6 +38,7 @@ class Eightyg extends MY_Controller {
 			for ($i=0; $i < sizeof($eightyg_ids); $i++) { 
 				//print_r($this->viewData['eightyg_data'][$i]);
 				$this->generate80G_PDF($this->viewData['eightyg_data'][$i]);
+				$this->sendemail($this->viewData['eightyg_data'][$i]);
 			}
 		}		
 
@@ -133,15 +135,15 @@ class Eightyg extends MY_Controller {
         }
 	}
 
-	public function generatepdf ($data) {
-		$details['firstname'] = 'first';
-		$details['lastname'] = '';
-		$details['city'] = 'hyd';
-		$details['state'] = 'ap';
-		$details['pan'] = 'pan1';
-		$details['amount'] = '321.12';
-		$this->generate80G_PDF($details, '123', 'Donation towards COVID-19 Relief Work');
-	}
+	// public function generatepdf ($data) {
+	// 	$details['firstname'] = 'first';
+	// 	$details['lastname'] = '';
+	// 	$details['city'] = 'hyd';
+	// 	$details['state'] = 'ap';
+	// 	$details['pan'] = 'pan1';
+	// 	$details['amount'] = '321.12';
+	// 	$this->generate80G_PDF($details, '123', 'Donation towards COVID-19 Relief Work');
+	// }
 	
 	public function generate80G_PDF($details) {
 		$city = '';
@@ -290,6 +292,123 @@ class Eightyg extends MY_Controller {
 		$get_paise = ($amount_after_decimal > 0) ? "And " . ($change_words[$amount_after_decimal / 10] . "
 	   " . $change_words[$amount_after_decimal % 10]) . ' Paise' : '';
 		return ($implode_to_Rupees ? $implode_to_Rupees . 'Rupees ' : '') . $get_paise;
+	}
+
+	public function sendemail($details) {
+		$message ="Hello ".$details->donor_name.",<br/><br/>
+Thanks for your donation.<br/><br/>
+<strong>Your Donation Details:</strong><br/>
+Amount: ".$details->sum_monthly_contribution."<br/>
+Transaction ID: ".$details->ref_details."<br/><br/>
+Please find the attachment for <strong>80G Certificate</strong>,
+
+Looking forward for your continued support.<br/><br/>
+-------------------------------<br/>
+Thanks and Regards,<br/>
+Manager - Accounts<br/>
+United Way of Hyderabad<br/>
+Phone : 040 40042010/11<br/>
+email : accounts@unitedwayhyderabad.org<br/>
+web: www.unitedwayhyderabad.org";
+		/*
+		* This example shows settings to use when sending via Google's Gmail servers.
+		* The IMAP section shows how to save this message to the 'Sent Mail' folder using IMAP commands.
+		*/
+
+		//SMTP needs accurate times, and the PHP time zone MUST be set
+		//This should be done in your php.ini, but this is how to do it if you don't have access to that
+		//date_default_timezone_set('Etc/UTC');
+
+		//Create a new PHPMailer instance
+		$mail = new PHPMailer;
+
+		//Tell PHPMailer to use SMTP
+		$mail->isSMTP();
+
+		//Enable SMTP debugging
+		// 0 = off (for production use)
+		// 1 = client messages
+		// 2 = client and server messages
+		$mail->SMTPDebug = 2;
+
+		//Ask for HTML-friendly debug output
+		$mail->Debugoutput = 'html';
+
+		//Set the hostname of the mail server
+		$mail->Host = 'smtp.gmail.com';
+		// use
+		// $mail->Host = gethostbyname('smtp.gmail.com');
+		// if your network does not support SMTP over IPv6
+
+		//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+		$mail->Port = 587;
+
+		//Set the encryption system to use - ssl (deprecated) or tls
+		$mail->SMTPSecure = 'tls';
+
+		//Whether to use SMTP authentication
+		$mail->SMTPAuth = true;
+
+		//Username to use for SMTP authentication - use full email address for gmail
+		$mail->Username = "noreply@unitedwayhyderabad.org";
+
+		//Password to use for SMTP authentication
+		$mail->Password = "<>3rPEr421";
+
+		//Set who the message is to be sent from
+		$mail->setFrom('admin@unitedwayhyderabad.org', 'UWH');
+
+		//Set an alternative reply-to address
+		$mail->addReplyTo('admin@unitedwayhyderabad.org', 'First Last');
+
+		$mail->addCC('adityanathc@gathi.com');
+
+		//Set who the message is to be sent to
+		$mail->addAddress('suresh@unitedwayhyderabad.org', 'suresh');
+
+		//Set the subject line
+		$mail->Subject = "Thank you for your Donation to United Way of Hyderabad";
+
+		//Read an HTML message body from an external file, convert referenced images to embedded,
+		//convert HTML into a basic plain-text alternative body
+		//$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+		$mail->msgHTML($message);
+
+		//Replace the plain text body with one created manually
+		//$mail->AltBody = 'This is a plain-text message body';
+
+		//Attach an image file
+		$mail->addAttachment('80g_certificates/'.$details->receipt_no.'.pdf');
+
+		//send the message, check for errors
+		if (!$mail->send()) {
+			echo "Mailer Error: " . $mail->ErrorInfo;
+		} else {
+			echo "Message sent!";
+			//Section 2: IMAP
+			//Uncomment these to save your message in the 'Sent Mail' folder.
+			if ($this->save_mail($mail)) {
+				echo "Message saved!";
+			}
+		}
+	}
+
+	//Section 2: IMAP
+	//IMAP commands requires the PHP IMAP Extension, found at: https://php.net/manual/en/imap.setup.php
+	//Function to call which uses the PHP imap_*() functions to save messages: https://php.net/manual/en/book.imap.php
+	//You can use imap_getmailboxes($imapStream, '/imap/ssl') to get a list of available folders or labels, this can
+	//be useful if you are trying to get this working on a non-Gmail IMAP server.
+	public function save_mail($mail) {
+		//You can change 'Sent Mail' to any other folder or tag
+		$path = "{imap.gmail.com:993/imap/ssl}[Gmail]/Sent Mail";
+
+		//Tell your server to open an IMAP connection using the same username and password as you used for SMTP
+		$imapStream = imap_open($path, $mail->Username, $mail->Password);
+
+		$result = imap_append($imapStream, $path, $mail->getSentMIMEMessage());
+		imap_close($imapStream);
+
+		return $result;
 	}
 
 	public function pdfemail () {
